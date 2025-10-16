013dbe589cce4945e38c955a15ad35feb90995f9
pratikdevelop <pratikraut88895@gmail.com>
2025-10-16 16:38:17 +0530

    fix(environment): remove trailing slash from apiUrl

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: "https://finance-management-application-3omi.onrender.com/",
+    apiUrl: "https://finance-management-application-3omi.onrender.com",
     appTitle: 'FinDash'
 };
